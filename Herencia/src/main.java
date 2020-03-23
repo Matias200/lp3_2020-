f305387c9b854a8483d5f391179ee9cad34b5cff
Matias200 <matiasrivas007@gmail.com>
2020-03-23 15:35:56 -0400

    Tarea2

---
+++ Herencia/src/main.java
@@ -0,0 +1,11 @@
+
+public class main {
+	public static void main(String[] args) {
+		Cliente cliente = new Cliente("Ernesto","Ramos",19,1001);
+		Vendedor vendedor = new Vendedor("Milagros","Garay",25,01);
+	
+		cliente.Mostrar(); 
+		vendedor.Mostrar();
+	}
+
+}
